36748f20e01dcd6b200c288a0204c8779563e471
ajochope <ocalles@gmail.com>
2015-01-10 07:26:36 +0100

    IOS 8 Sift tutorial
    To do list app with basic persistence and delete task
    www.udemy.com/complete-ios-developer-course

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+// this array contains the items in the Table View list
+var toDoList:[String] = []
 
+
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+   
+   @IBOutlet var tasksTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       // self.tasksTable.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,57 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    
+    {
+        // table view take the size of the array length
+        return toDoList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        //var cell:UITableViewCell = self.tasksTable.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
+        
+        // the indexPath.row take the correct item for each row in the table from the array
+        cell.textLabel?.text = toDoList[indexPath.row]
+        return cell
+    }
+    // this func run just before the view appear 
+    // usefull to run and reaload items when move over screens
+    override func viewWillAppear(animated: Bool) {
+        
+        // call to persitance to fill the table view if exist
+        
+        if var storedToDoItems: AnyObject? = NSUserDefaults.standardUserDefaults().objectForKey("toDoItems") {
+            
+            toDoList = [] // first emptying the array
+            for var i = 0; i<storedToDoItems?.count; ++i {
+                var stringStored = storedToDoItems as Array <String!> // Create new var stringStored (type string) from the storedToDoItems (object)
+                toDoList.append(stringStored[i] as NSString)
+            }
+        }
+        tasksTable.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+    {
+        // look for user Delete action 
+        if (editingStyle == UITableViewCellEditingStyle.Delete){
+            toDoList.removeAtIndex(indexPath.row)
+            storeToDoList()
+            tasksTable.reloadData()
+        }
+    
+    }
+    
+    private func storeToDoList(){
+        // Basic persistance for the list items
+        let fixedToDoList = toDoList
+        NSUserDefaults.standardUserDefaults().setObject(fixedToDoList, forKey: "toDoItems")
+        NSUserDefaults.standardUserDefaults().synchronize()
+        
+    }
+    
 }
 
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,8 +8,36 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var toDoItem: UITextField!
+    
+    @IBOutlet weak var lbEmpty: UILabel!
+    @IBAction func addItem(sender: AnyObject) {
+        if toDoItem.text == "" {
+            lbEmpty.text = "No item name entered"
+        } else {
+           toDoList.append(toDoItem.text)
+        }
+        
+        
+        // Basic persistance for the list items
+        storeToDoList()
+        
+        self.view.endEditing(true)
+        toDoItem.text = ""
+        
+    }
+    
+    private func storeToDoList(){
+        // Basic persistance for the list items
+        let fixedToDoList = toDoList
+        NSUserDefaults.standardUserDefaults().setObject(fixedToDoList, forKey: "toDoItems")
+        NSUserDefaults.standardUserDefaults().synchronize()
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +47,18 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool
+    {
+        // this hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // needed for hide the keyboard
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
 
 }
 
